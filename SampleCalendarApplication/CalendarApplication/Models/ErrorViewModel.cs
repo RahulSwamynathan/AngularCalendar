62dd19621c0f96db903346a1f4fe031865c2fd07
Rahul Swamynathan <Rahul.S@customeranalytics.com>
2019-05-09 13:12:18 +0530

    Base version

---
+++ SampleCalendarApplication/CalendarApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CalendarApplication.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

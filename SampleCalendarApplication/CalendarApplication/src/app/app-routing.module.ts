7866558025e319983ff89abf60848d577d66b5ed
Rahul Swamynathan <Rahul.S@customeranalytics.com>
2019-05-09 16:21:43 +0530

    Calender added

--- SampleCalendarApplication/CalendarApplication/src/app/app-routing.module.ts
+++ SampleCalendarApplication/CalendarApplication/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoComponent } from './components/demo/demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "demo", component: DemoComponent,
+  },
+  {
+    path: "", component: DemoComponent, pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
